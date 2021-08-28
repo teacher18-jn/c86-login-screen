09e96b5e6784c3e246ab9e064abb6017693d15d8
teacher18-jn <84465689+teacher18-jn@users.noreply.github.com>
2021-08-29 00:46:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyClmIJ3R3YXBfSE1PwHqBinUjPztYXrjQc",
+    authDomain: "storytelling-app-b67e1.firebaseapp.com",
+    projectId: "storytelling-app-b67e1",
+    storageBucket: "storytelling-app-b67e1.appspot.com",
+    messagingSenderId: "566197621086",
+    appId: "1:566197621086:web:28a64ef213033a63f77fcf"
+  };
